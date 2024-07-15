4a769754e0b47204fb5e28f474d6ada9464bd0ce
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-07-15 21:27:26 +0800

    lol

--- test/src/dev-commands.ts
+++ test/src/dev-commands.ts
@@ -4,6 +4,7 @@ export default new RecipleDevCommands({
     allowExecuteInDms: true,
     allowExecuteInNonDevGuild: true,
     allowNonDevUserExecuteInDevGuild: false,
+    deployInProduction: true,
     devGuilds: ['876039624814899220'],
     devUsers: ['749120018771345488'],
     logger: undefined
